0b5aef96ef56a41d1e871ef412610a10000e02ec
Caesar Kabalan <caesar.kabalan@gmail.com>
2021-01-07 13:51:27 -0700

    Start Basic Item Completion Code

--- js/main.js
+++ js/main.js
@@ -1,14 +1,45 @@
-$(() => {
+$(document).ready(function() {
+    // On Load Code
+
+    // Functions
+    function getMissingItemIDs(itemStats) {
+        console.log(itemStats);
+        missingIDs = [];
+        itemStats.forEach(function(item) {
+            if (item.stats[0] == 0) {
+                missingIDs.push(item.itemID);
+            }
+        });
+        return missingIDs;
+    }
+    function itemLink(itemID, brackets = false) {
+        itemName = items[itemID].name;
+        articleName = itemName.replace('#', '')
+        return '<a href="https://wiki.melvoridle.com/index.php?title=' + articleName + '" target="_new">' + (brackets?'[':'') + itemName + (brackets?']':'') + '</a>';
+    }
+
+    function generateSourceString(itemID) {
+        sourceStr = items[itemID].category;
+        if (items[itemID].hasOwnProperty('itemsRequired')) {
+            itemStrs = []
+            items[itemID].itemsRequired.forEach(function(reqs) {
+                itemStrs.push(itemLink(reqs[0], true) + ' x ' + reqs[1])
+            });
+            sourceStr = itemStrs.join('<br/>')
+        }
+        return sourceStr;
+    }
+
+    // Events
     $('#saveImport').on('blur', () => {
         // https://stackoverflow.com/a/41106346/606974
         saveJSON = JSON.parse(pako.ungzip(Uint8Array.from(atob($('#saveImport').val()), c => c.charCodeAt(0)), { to: 'string' }));
-        saveData = JSON.stringify(saveJSON, null, 2);
-        console.log(saveJSON.itemsAlreadyFound);
-        console.log(items)
-        items.forEach(function(item, index) {
-            console.log(item)
-            console.log(index)
+        //saveData = JSON.stringify(saveJSON, null, 2);
+        missingItemIDs = getMissingItemIDs(saveJSON.itemStats);
+        console.log(missingItemIDs);
+        missingItemIDs.forEach(function(itemID) {
+            console.log(items[itemID]);
+            $('#tableItems tr:last').after('<tr><td>' + itemID + '</td><td>' + itemLink(itemID) + '</td><td>' + generateSourceString(itemID) + '</td></tr>');
         });
-        //missingItems = allItemIds.filter(x => !(saveJSON.itemsAlreadyFound).includes(x));
     });
 });
