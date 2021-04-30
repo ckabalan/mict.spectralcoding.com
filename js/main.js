28651b3c2ddbcf776593836d91db131b5448f523
Joel Teichroeb <joel@teichroeb.net>
2021-04-29 22:07:36 -0400

    Use the paste event

--- js/main.js
+++ js/main.js
@@ -289,8 +289,8 @@ $(document).ready(function() {
         resetMissingTables();
         processSave();
     });
-    $('#saveImport').on('blur', () => {
-        processSave();
+    $('#saveImport').on('paste', (e) => {
+        processSave(e.originalEvent.clipboardData.getData('text'));
     });
     $('#filter').on('input', function(){
         // https://stackoverflow.com/a/41554434/606974
@@ -322,9 +322,12 @@ $(document).ready(function() {
         $('#filter').val('');
         $('#missingWrapper table tbody').html('');
     }
-    function loadSaveData() {
+    function loadSaveData(saveData) {
+        if (saveData === undefined) {
+            saveData = $('#saveImport').val();
+        }
         // Quit if blank save
-        if (!$('#saveImport').val()) {
+        if (!saveData) {
             $('#saveLoadAlert').text('');
             $('#saveLoadAlert').addClass('d-none');
             return false;
@@ -332,7 +335,7 @@ $(document).ready(function() {
         // Quit if corrupt save (unable to un-base64 and un-gzip).
         try {
             // https://stackoverflow.com/a/41106346/606974
-            saveJSON = JSON.parse(pako.ungzip(Uint8Array.from(atob($('#saveImport').val()), c => c.charCodeAt(0)), { to: 'string' }));
+            saveJSON = JSON.parse(pako.ungzip(Uint8Array.from(atob(saveData), c => c.charCodeAt(0)), { to: 'string' }));
         }
         catch(err) {
             $('#saveLoadAlert').html('<span style="font-weight:bold">Save data corrupt!</span><br/>Did you accidentally add any text to the save data? Steps for retreiving sava data are found by clicking the <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/></svg> icon above.');
@@ -360,8 +363,8 @@ $(document).ready(function() {
         saveJSON['itemStats'] = saveJSON['itemStats'] ?? [];
         return saveJSON;
     }
-    function processSave() {
-        var saveJSON = loadSaveData();
+    function processSave(saveData) {
+        var saveJSON = loadSaveData(saveData);
         if (!saveJSON) {
             // Hide and and reset tables
             $('#missingWrapper').addClass('d-none');
