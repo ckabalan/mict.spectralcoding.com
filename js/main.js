53a506d2904ba18a9099d1cabf37c079014a636d
Caesar Kabalan <caesar.kabalan@gmail.com>
2021-08-13 21:30:51 -0700

    Update for Alpha v0.21, Notably New Save Format

--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
     // Functions
     function getMissingItemIDs(itemStats) {
         var missingIDs = []
-        itemStats.forEach(function(item) {
-            if (item.stats[0] == 0) {
-                missingIDs.push(item['itemID'].toString())
+        itemStats.forEach(function(item, index) {
+            if (item == 0 && index%3 == 0) {
+                missingIDs.push((index/3).toString())
             }
         });
         return missingIDs;
@@ -16,9 +16,9 @@ $(document).ready(function() {
     function getMissingMonsterIDs(monsterStats) {
         var missingIDs = Object.keys(melvorData['monsters']);
         var missingIDs = []
-        monsterStats.forEach(function(monster) {
-            if (monster.stats[2] == 0) {
-                missingIDs.push(monster['monsterID'].toString())
+        monsterStats.forEach(function(monster, index) {
+            if (index%10 == 2 && monster == 0) {
+                missingIDs.push(((index - index%10)/10).toString())
             }
         });
         return missingIDs;
@@ -76,13 +76,13 @@ $(document).ready(function() {
         return (image?'<img src="' + CDNPrefix + melvorData['skills'][skillID]['media'] + '" />':'') + '<a href="https://wiki.melvoridle.com/index.php?title=' + articleName + '" target="_new">' + (brackets?'[':'') + skillName + (brackets?']':'') + '</a>';
     }
     function combatAreaLink(combatAreaID, brackets = false, image = false) {
-        combatAreaName = melvorData['combatAreas'][combatAreaID]['areaName'];
+        combatAreaName = melvorData['combatAreas'][combatAreaID]['name'];
         articleName = combatAreaName.replace('#', '');
         // This may be a bug, but the Dungeon asset data has the CDN prefix already specified
         return (image?'<img src="' + melvorData['combatAreas'][combatAreaID]['media'] + '" />':'') + '<a href="https://wiki.melvoridle.com/index.php?title=' + articleName + '" target="_new">' + (brackets?'[':'') + combatAreaName + (brackets?']':'') + '</a>';
     }
     function slayerAreaLink(slayerAreaID, brackets = false, image = false) {
-        slayerAreaName = melvorData['slayerAreas'][slayerAreaID]['areaName'];
+        slayerAreaName = melvorData['slayerAreas'][slayerAreaID]['name'];
         articleName = slayerAreaName.replace('#', '');
         // This may be a bug, but the Dungeon asset data has the CDN prefix already specified
         return (image?'<img src="' + melvorData['slayerAreas'][slayerAreaID]['media'] + '" />':'') + '<a href="https://wiki.melvoridle.com/index.php?title=' + articleName + '" target="_new">' + (brackets?'[':'') + slayerAreaName + (brackets?']':'') + '</a>';
@@ -327,7 +327,18 @@ $(document).ready(function() {
         // Quit if corrupt save (unable to un-base64 and un-gzip).
         try {
             // https://stackoverflow.com/a/41106346/606974
-            saveJSON = JSON.parse(pako.ungzip(Uint8Array.from(atob(saveData), c => c.charCodeAt(0)), { to: 'string' }));
+            saveJSONRaw = JSON.parse(pako.ungzip(Uint8Array.from(atob(saveData), c => c.charCodeAt(0)), { to: 'string' }));
+            //console.log("Raw:")
+            //console.log(saveJSONRaw)
+            saveJSON = {}
+            saveJSON['itemStats'] = (saveJSONRaw['ns'] && saveJSONRaw['ns'][3] && saveJSONRaw['ns'][3][0]) ?? [];
+            saveJSON['monsterStats'] = (saveJSONRaw['s'] && saveJSONRaw['s'][21]) ?? [];
+            saveJSON['petUnlocked'] = (saveJSONRaw['s'] && saveJSONRaw['s'][22]) ?? [];
+            saveJSON['accountGameVersion'] = (saveJSONRaw['o'] && saveJSONRaw['o'][4].replaceAll('"', '')) ?? 'Unknown Game Version';
+            //console.log("Fixed:")
+            //console.log(saveJSON)
+            // Free up the old save data
+            saveJSONRaw = null
         }
         catch(err) {
             $('#saveLoadAlert').html('<span style="font-weight:bold">Save data corrupt!</span><br/>Did you accidentally add any text to the save data? Steps for retreiving sava data are found by clicking the <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/></svg> icon above.');
